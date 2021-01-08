ed5a38063b5391702021da718a7143e609522bcb
Jonathan Camile <jonathan.camile@protonmail.com>
2021-01-09 00:04:38 +0100

    feat(messages): Add scaffold for Message object

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2021_01_08_190456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "messages", force: :cascade do |t|
+    t.boolean "enabled"
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.boolean "enabled", default: true
     t.string "firstname"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_01_08_190456) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
