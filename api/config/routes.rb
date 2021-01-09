f3d30cc6ea9ae045bc77ce0f5bc3c20734aaa24c
Jonathan Camile <jonathan.camile@protonmail.com>
2021-01-09 14:12:22 +0100

    feat: Add Doorkeep gem to manage OAuth2

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
   devise_for :users
   resources :messages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'
+  namespace :api do
+    resources :messages
+  end
 end
